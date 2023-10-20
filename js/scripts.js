baee9db3c08b815e8d9cd10a3e072d9ec8f3c65d
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2023-10-20 10:48:04 -0700

    add second dropdown with semi working js

--- js/scripts.js
+++ js/scripts.js
@@ -11,6 +11,10 @@ function selectLang(event) {
 
   const langSelect = document.getElementById("frontOrBack").value;
   console.log(langSelect);
+
+  const appSelect = document.getElementById("appSelect").value;
+  console.log(appSelect);
+  
   const interestSelect = document.querySelector("input[name='interest']:checked").value;
   console.log(interestSelect);
 }
