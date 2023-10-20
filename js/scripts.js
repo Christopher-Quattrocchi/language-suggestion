42cd0b1b271eccae88ec0934c8f0798683b565fb
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2023-10-20 12:00:00 -0700

    trying to unhide divs based on the selection made in the first drop down menu. Send help

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,39 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   const form = document.getElementById("lang-select");
   form.addEventListener("submit", saveChoice);
 });
 
 function saveChoice(event) {
   event.preventDefault();
-  console.log(event);
-  console.log(event.target);
-  
+  // console.log(event);
+  // console.log(event.target);
+
 
   const langSelect = document.getElementById("frontOrBack").value;
-  console.log(langSelect);
+  // return langSelect;
 
   const appSelect = document.getElementById("appSelect").value;
-  console.log(appSelect);
+  // return appSelect;
 
   const interestSelect = document.querySelector("input[name='interest']:checked").value;
-  console.log(interestSelect);
+
+  console.log(langSelect, appSelect, interestSelect);
+
+
+  // return interestSelect;
+  displayLangStack(langSelect);
 }
 
+
+function displayLangStack(langSelect) {
+  if (langSelect === "frontend") {
+    let frontend = document.getElementById("front-end");
+    frontend.removeAttribute("class");
+  } else if (langSelect === "backend") {
+    let backend = document.getElementById("back-end");
+    backend.removeAttribute("class");
+  } else if (langSelect === "fullstack") {
+    let fullstack = document.getElementById("full-stack");
+    fullstack.removeAttribute("class");
+  }
+}
